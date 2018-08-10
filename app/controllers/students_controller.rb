450133381138a2fbb1f51511e8c2f2fced004808
Jason Pitts <juniorjr083@gmail.com>
2018-08-10 21:09:06 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,7 @@ class StudentsController < ApplicationController
  
  
  def activate
-   @student = Student.find
+   @student = Student.find(params[:id])
     if @student.active
       @student.update(active: false)
     else
